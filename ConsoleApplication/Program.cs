a6d3dd117d1087bad71c01abe7eaf81bc2c77e91
Kwansiri12 <kwansiri.thong15@gmail.com>
2025-11-19 09:51:16 +0700

    Add project files.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            int height = 0;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+            Console.Write("please input full name(Eng):");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hello,Welcome" + name);
+
+            Console.Write("Please input you height(cm):");
+            height = Convert.ToInt16(Console.ReadLine());
+            double weight = height - 100;
+
+            Console.WriteLine("Your ideal weight is" + weight.ToString());
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to end program");
+            Console.WriteLine();
+            
+
+        }
+    }
+}
